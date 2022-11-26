8fe8010e9cce3a799fa79a11b126177b4ee458b7
shadowelite <alansedp@gmail.com>
2022-11-26 21:19:51 +0530

    init

---
+++ day1/index.js
@@ -0,0 +1,57 @@
+/* hello world*/
+console.log("hello world");
+console.log("hey all")
+
+
+//this is an alter
+window.alert("You Have Been Hacked")
+
+let x = 5;
+let firstname = "Bro";
+let ans = x*5;
+
+//boolen true or false
+let student = true;
+
+
+console.log(ans);
+console.log(firstname);
+console.log(student)
+
+age = "18"
+age = age + 1
+
+console.log("Its is not age : ",age);
+
+//intract with html
+//change values of html Document
+
+document.getElementById("p1").innerHTML = "Hello " + firstname;
+
+document.getElementById("p2").innerHTML = "Your age is not " + age + " Year old";
+
+document.getElementById("p3").innerHTML = "5x5 is " + ans + " And Its " + student;
+
+//math
+
+a = 20;
+a = a*2;
+
+console.log(a);
+
+b = 5;
+b = b + 5;
+
+console.log(b);
+
+
+c = 10;
+c = c - 10;
+
+console.log(c);
+
+
+d = 10;
+d = d / 2;
+
+console.log(d);
